ea204ca0a4eed7630ae9a3c4f48bfb94218657f7
Filippo Guerranti <l4place@gmail.com>
2021-04-10 13:17:32 +0200

    Test convolve functions with chronometer

--- test/testTensor.cpp
+++ test/testTensor.cpp
@@ -7,17 +7,17 @@ int main(int argc, char const *argv[]){
     
     typedef float DType;
 
-    Tensor<DType> image{7,3,10,10,tensor::init::INCR};
+    Tensor<DType> image{7,3,1024,1024,tensor::init::INCR};
     Tensor<DType> kernel{10,3,3,3,tensor::init::INCR};
 
     auto stride = 1;
     auto padding = 0;
     auto nThreads = 4;
 
-    Tensor<DType> output1 = image.convolve(kernel, stride, padding, nThreads);
-    Tensor<DType> output2 = image.convolve(kernel, stride, padding);
-
-    std::cout << (output1 == output2) << std::endl;
+    Tensor<DType> output1 = image.convolveNaive(kernel, stride, padding);
+    Tensor<DType> output2 = image.convolveParallelHo(kernel, stride, padding, nThreads);
+    Tensor<DType> output3 = image.convolveParallelCo(kernel, stride, padding, nThreads);
+    Tensor<DType> output4 = image.convolveParallelEo(kernel, stride, padding, nThreads);
 
     return 0;
 }
